435cf98d80d2d5084f2c910ed9057c77e906b646
SamuelGTL <60748162+SamuelGTL@users.noreply.github.com>
2020-05-25 18:19:30 -0500

    Add files via upload

--- ConexionLocal.php
+++ ConexionLocal.php
@@ -1,5 +1,18 @@
 <$php
 function conecta()
 {
-
+function Correr()
+      {
+ $servidor="localhost";
+    $usuario="root";
+    $clave="sdgdsg";
+    $BaseDeDatos="Formulario";
+      $enlace=mysqli_connect($servidor,$usuario,$clave,$BaseDeDatos);
+      if(!enlace)
+      {
+      echo"Error en la conexion con el servidor";
+      }
+      }
+      
+      
 }
